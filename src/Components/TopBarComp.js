92151864bbc6c5a298065e516edb90754442eeeb
cleon5 <cleon5@ucol.mx>
2022-09-16 22:02:24 -0500

    Button filter update

--- src/Components/TopBarComp.js
+++ src/Components/TopBarComp.js
@@ -1,14 +1,17 @@
-import { View, Text, StyleSheet, Button,  } from 'react-native'
+import { View, Text, StyleSheet, Button, Pressable, TouchableOpacity  } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import { FontAwesome } from "@expo/vector-icons";
 import { FondoColor } from '../Constants/colors';
 
 const TopBarComp = ({parentCallback}) => {
-  const [Mostrar, setMostrar] = useState(true);
   
   return (
     <View style={styles.container}>
-      <Button title="Search" onPress={() => parentCallback(true)}/>
+      <Button title="Search" style={styles.Button} onPress={() => parentCallback(true)}/>
+
+      <TouchableOpacity style={styles.Button} onPress={() => parentCallback(true)}>
+          <Text style={styles.txt}>Search</Text>
+        </TouchableOpacity>
 
       <Text style={styles.txt}>TopBarComp</Text>
       <FontAwesome name="gamepad" size={24} style={styles.icon} />
@@ -34,6 +37,16 @@ const styles = StyleSheet.create({
     icon:{
       color: 'white',
       textAlignVertical: 'center',
+    },
+    Button: {
+      padding : 1,
+      margin : 10,
+      height : 40,
+      
+      backgroundColor: 'blue',
+      textAlignVertical: 'center',
+      alignSelf: 'center',
+      color : 'black',
     }
 })
 
